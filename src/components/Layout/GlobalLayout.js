747df7540f4e1b778f073a1da4b93df0d76bae32
David Luhr <david.luhr@dockyard.com>
2018-10-25 12:29:19 -0600

    14 Add fonts

--- src/components/Layout/GlobalLayout.js
+++ src/components/Layout/GlobalLayout.js
@@ -1,16 +1,86 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { injectGlobal } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
-import { fontFamilySans } from '../../theme/settings';
+import {
+  fontFamilySans,
+  weightLight,
+  weightMedium,
+  weightRegular,
+  weightSemiBold,
+  weightBold,
+} from '../../theme/settings';
 
 // Typography
+import ibmPlexSansLightTTF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Light.ttf';
+import ibmPlexSansLightWOFF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Light.woff';
+import ibmPlexSansLightWOFF2 from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Light.woff2';
+
+import ibmPlexSansRegularTTF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Regular.ttf';
+import ibmPlexSansRegularWOFF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Regular.woff';
+import ibmPlexSansRegularWOFF2 from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Regular.woff2';
+
+import ibmPlexSansMediumTTF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Medium.ttf';
+import ibmPlexSansMediumWOFF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Medium.woff';
+import ibmPlexSansMediumWOFF2 from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Medium.woff2';
+
+import ibmPlexSansSemiBoldTTF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-SemiBold.ttf';
+import ibmPlexSansSemiBoldWOFF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-SemiBold.woff';
+import ibmPlexSansSemiBoldWOFF2 from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-SemiBold.woff2';
+
+import ibmPlexSansBoldTTF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Bold.ttf';
+import ibmPlexSansBoldWOFF from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Bold.woff';
+import ibmPlexSansBoldWOFF2 from '../../assets/fonts/ibm-plex-sans/IBMPlexSans-Bold.woff2';
 
 // Components
 import SkipNav from '../A11y/SkipNav';
 
-injectGlobal`
+const GlobalStyle = createGlobalStyle`
   /* Typography */
+  @font-face {
+    font-family: 'IBM Plex Sans';
+    font-style: normal;
+    font-weight: ${weightLight};
+    src: url(${ibmPlexSansLightWOFF2}) format('woff2'),
+      url(${ibmPlexSansLightWOFF}) format('woff'),
+      url(${ibmPlexSansLightTTF}) format('ttf');
+  }
+
+  @font-face {
+    font-family: 'IBM Plex Sans';
+    font-style: normal;
+    font-weight: ${weightRegular};
+    src: url(${ibmPlexSansRegularWOFF2}) format('woff2'),
+      url(${ibmPlexSansRegularWOFF}) format('woff'),
+      url(${ibmPlexSansRegularTTF}) format('ttf');
+  }
+
+  @font-face {
+    font-family: 'IBM Plex Sans';
+    font-style: normal;
+    font-weight: ${weightMedium};
+    src: url(${ibmPlexSansMediumWOFF2}) format('woff2'),
+      url(${ibmPlexSansMediumWOFF}) format('woff'),
+      url(${ibmPlexSansMediumTTF}) format('ttf');
+  }
+
+  @font-face {
+    font-family: 'IBM Plex Sans';
+    font-style: normal;
+    font-weight: ${weightSemiBold};
+    src: url(${ibmPlexSansSemiBoldWOFF2}) format('woff2'),
+      url(${ibmPlexSansSemiBoldWOFF}) format('woff'),
+      url(${ibmPlexSansSemiBoldTTF}) format('ttf');
+  }
+
+  @font-face {
+    font-family: 'IBM Plex Sans';
+    font-style: normal;
+    font-weight: ${weightBold};
+    src: url(${ibmPlexSansBoldWOFF2}) format('woff2'),
+      url(${ibmPlexSansBoldWOFF}) format('woff'),
+      url(${ibmPlexSansBoldTTF}) format('ttf');
+  }
 
   /* Reset */
   /* http://meyerweb.com/eric/tools/css/reset/
@@ -83,6 +153,7 @@ injectGlobal`
 
 const GlobalLayout = ({ children }) => (
   <>
+    <GlobalStyle />
     <SkipNav anchor="mainContent">Skip to main content</SkipNav>
     <div>{children}</div>
   </>

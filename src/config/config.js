00504899860045955df5b324de6bcf9ab8f3ac38
David Luhr <davidc.luhr@gmail.com>
2018-10-26 20:53:56 -0600

    #17 Add Coming Soon screen (#20)

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,11 @@
+import ogImage from '../assets/images/index/build-ux-index-og-image.jpg';
+
 const config = {
-  title: 'Build UX | TEST TITLE',
-  description: 'TEST DESCRIPTION',
+  title: 'Build UX | Videos on Expert HTML, CSS, and Accessilbity',
+  description:
+    'Build UX is a video series on expert HTML, CSS, and accessibility. Watch and learn how to take web designs from static mockups to robust user experiences.',
   url: 'https://buildux.co',
-  image: 'https://buildux.co/image',
+  image: ogImage,
   logo: 'https://buildux.co/icons/icon-512x512.png',
   twitter: '@build_ux',
   fbAppID: '',
